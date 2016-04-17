6c152aff934134e9460cd15ba4cd75fb56ce9cd2
Tan <tan@ring.md>
2016-04-17 12:46:23 +0800

    use code to get access token and account information

--- app/controllers/account_kit/sessions_controller.rb
+++ app/controllers/account_kit/sessions_controller.rb
@@ -3,5 +3,22 @@ module AccountKit
     def new
       @user = User.new
     end
+
+    def create
+      @access_token, @expires_at, @user_id = access_token(params[:code])
+      response = AccountKit.me(@access_token)
+      json_response = JSON.parse(response.body)
+      @email_arddess = json_response['email']['address'] if json_response['email']
+      @phone_number = json_response['phone']['number'] if json_response['phone']
+      render 'signed_in'
+    end
+
+    private
+
+    def access_token(code)
+      response = AccountKit.access_token(code)
+      json_response = JSON.parse(response.body)
+      [json_response['access_token'], json_response['token_refresh_interval_sec'], json_response['id']]
+    end
   end
 end
