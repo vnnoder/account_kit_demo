aed8129058c0324c332efe8bfe8d02291f95fcd8
Tan <tan@ring.md>
2016-04-17 12:53:23 +0800

    uses Account Kit Demo app id and secret

--- config/initializers/account_kit.rb
+++ config/initializers/account_kit.rb
@@ -1,5 +1,5 @@
 AccountKit.configure do |config|
-  config.app_id = '377314202423263'
-  config.app_secret = '30a55b0dec2e34c78a539bc055e67766'
+  config.app_id = '981076428655293'
+  config.app_secret = '5e6353199a4fd713bb8b9be58428f42d'
   config.api_version = 'v1.0'
 end
