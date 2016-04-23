27b4bc8f265600faa9f1fbec5b4f6e2054d20186
Tan <tan@ring.md>
2016-04-23 10:14:32 +0800

    set app_id and app_secret based on params require_app_secret

--- app/controllers/account_kit/sessions_controller.rb
+++ app/controllers/account_kit/sessions_controller.rb
@@ -1,11 +1,12 @@
 module AccountKit
   class SessionsController < ApplicationController
+    before_action :config_app_secret, only: [:new]
+
     def new
       @user = User.new
     end
 
     def create
-      require_app_secret
       @access_token, @expires_at, @user_id = access_token(params[:code])
       response = AccountKit.me(@access_token)
       json_response = JSON.parse(response.body)
@@ -16,9 +17,11 @@ module AccountKit
 
     private
 
-    def require_app_secret
+    def config_app_secret
       AccountKit.configure do |config|
         config.require_app_secret = params[:require_app_secret].present?
+        config.app_id = params[:app_id] || app_id_for(params[:require_app_secret].present?)
+        config.app_secret = params[:app_secret] || app_secret_for(params[:require_app_secret].present?)
       end
     end
 
@@ -26,6 +29,14 @@ module AccountKit
       response = AccountKit.access_token(code)
       json_response = JSON.parse(response.body)
       [json_response['access_token'], json_response['token_refresh_interval_sec'], json_response['id']]
+    end
+
+    def app_id_for(require_app_secret)
+      require_app_secret ? '1804778196417244' : '981076428655293'
+    end
+
+    def app_secret_for(require_app_secret)
+      require_app_secret ? '074f550a820718853e92422bb012b932' : '5e6353199a4fd713bb8b9be58428f42d'
     end
   end
 end
--- config/initializers/account_kit.rb
+++
@@ -1,5 +0,0 @@
-AccountKit.configure do |config|
-  config.app_id = '981076428655293'
-  config.app_secret = '5e6353199a4fd713bb8b9be58428f42d'
-  config.api_version = 'v1.0'
-end
